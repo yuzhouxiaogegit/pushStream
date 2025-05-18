23b2e42c2212d42d4d2cc71f62d072aebfb1f7c6
宇宙小哥 <52238373+yuzhouxiaogegit@users.noreply.github.com>
2025-05-18 23:17:46 +0800

    ffmpeg_install.sh

--- ffmpeg_install.sh
+++ ffmpeg_install.sh
@@ -3,7 +3,7 @@
 #=================================================
 #	System Required: CentOS 6/7/8
 #	Description: ffmpeg 安装脚本
-#	Version: 1.0
+#	Version: 1.1
 #	Author: 宇宙小哥
 #	Blog: https://github.com/yuzhouxiaogegit/pushStream
 #=================================================
@@ -29,21 +29,26 @@ echoTxtColor(){
 # 调用示例
 # echoTxtColor "您的文字颜色打印成功" "green"
 
-read -p "请输入你要下载ffmpeg的版本号，(直接回车下载5.1版本):" ffmpegVersion
+# 获取最新软件版本号码,非gitbub版本号也能获取,例如:ffmpeg等
+# $1 = 软件releases地址
+# $2 = 软件名称
+# 函数调用示例 getNewVersionNum 'https://github.com/fatedier/frp/releases/' 'frp'
+getNewVersionNum(){
+	wget --timeout=10 $1 -O temp_$2.txt && echo "$(grep -Eo $2.[0-9.]+ temp_$2.txt | grep -Eo [0-9.]+[0-9] | tail -n 1)" && rm -rf temp_$2.txt
+}
 
-if [[ $ffmpegVersion = "" ]]; then
-	ffmpegVersion="5.1"
-fi
 cd
 # 安装编译工具 yasm
-wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-tar -zxvf yasm-1.3.0.tar.gz
-cd yasm-1.3.0
+yasmVersion=getNewVersionNum 'http://www.tortall.net/projects/yasm/releases/' 'yasm' # 获取yasm最新版本号
+wget http://www.tortall.net/projects/yasm/releases/yasm-${yasmVersion}.tar.gz
+tar -zxvf yasm-${yasmVersion}.tar.gz
+cd yasm-${yasmVyasmVersion}
 ./configure
 make && make install
 
 # 下载并且安装 ffmpeg
 cd
+ffmpegVersion=getNewVersionNum 'http://www.ffmpeg.org/releases/' 'ffmpeg' # 获取yasm最新版本号
 wget http://www.ffmpeg.org/releases/ffmpeg-${ffmpegVersion}.tar.gz
 tar -zxvf ffmpeg-${ffmpegVersion}.tar.gz
 cd ffmpeg-${ffmpegVersion}
@@ -57,6 +62,5 @@ source /etc/profile
 cd
 rm -rf yasm-1.3.0.tar.gz
 rm -rf ffmpeg-${ffmpegVersion}.tar.gz
-
 echoTxtColor "ffmpeg 安装完成" "green"
 exit
